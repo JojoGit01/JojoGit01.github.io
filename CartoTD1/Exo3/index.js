45bff3b380b8303e09a01629b7cea08848dea320
Jonathan <jonathan-08@orange.fr>
2022-01-07 11:21:38 +0100

    Fix update

--- CartoTD1/Exo3/index.js
+++ CartoTD1/Exo3/index.js
@@ -1,44 +1,40 @@
 
-const ialpa = document.querySelector('#alpa')
-const ibeta = document.querySelector('#beta')
-const igamma = document.querySelector('#gamma')
+const iAlpa = document.querySelector('#alpha')
+const iBeta = document.querySelector('#beta')
+const iGamma = document.querySelector('#gamma')
 
-const x = document.querySelector('#X')
-const y = document.querySelector('#Y')
-const z = document.querySelector('#Z')
+const iX = document.querySelector('#X')
+const iY = document.querySelector('#Y')
+const iZ = document.querySelector('#Z')
 
 function handleOrientation(event) {
-    let alpha    = event.alpha;
-    let beta     = event.beta;
-    let gamma    = event.gamma;
+    let eAlpha    = event.alpha;
+    let eBeta     = event.beta;
+    let eGamma    = event.gamma;
   
-    ialpa.textContent = alpha
-    ibeta.textContent =  beta
-    igamma.textContent = gamma
+    iAlpa.textContent = eAlpha
+    iBeta.textContent =  eBeta
+    iGamma.textContent = eGamma
 }
 
 function handleAcceleration(event) {
+    let eX = event.accelerationIncludingGravity.x;
+    let eY = event.accelerationIncludingGravity.y;
+    let eZ = event.accelerationIncludingGravity.z;
 
-    alert("zeaz")
-
-
-    let ex = event.accelerationIncludingGravity.x;
-    let ey = event.accelerationIncludingGravity.y;
-    let ez = event.accelerationIncludingGravity.z;
-
-    x.textContent = ex
-    y.textContent = ey
-    z.textContent = ez
+    iX.textContent = eX
+    iY.textContent = eY
+    iZ.textContent = eZ
 }
   
 if(window.DeviceOrientationEvent) {
-    window.addEventListener("deviceorientation", handleOrientation, false);
+    window.addEventListener("deviceorientation", handleOrientation, true);
 } else {
     alert("Le naviguateur ne supporte pas l'événement deviceorientation")
 }
 
 if(window.DeviceMotionEvent) {
-    window.addEventListener("devicemotion", handleAcceleration, false);
+    document.addEventListener("devicemotion", handleAcceleration, true);
 } else {
     alert("Le naviguateur ne supporte pas l'évenement devicemotion")
 }
