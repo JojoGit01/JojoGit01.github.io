03473728cc50e51dc247f45eb9a4d5a73ba6d546
Jonathan <jonathan-08@orange.fr>
2022-01-08 17:18:14 +0100

    fix

--- CartoTD1/Exo4/index.js
+++ CartoTD1/Exo4/index.js
@@ -1,37 +1,30 @@
-const start = () => { 
-
-}
 
 const iX = document.querySelector('#x')
 const iY = document.querySelector('#y')
 const iEvent = document.querySelector('#event')
 
-function onTouch(event) {
-    event.preventDefault();
-    if (event.touches.length > 1 || (event.type == "touchend" && event.touches.length > 0))
-      return;
-  
-    var newEvt = document.createEvent("MouseEvents");
-    var type = null;
-    var touch = null;
-    switch (event.type) {
-      case "touchstart": type = "mousedown"; touch = event.changedTouches[0];
-      case "touchmove":  type = "mousemove"; touch = event.changedTouches[0];
-      case "touchend":   type = "mouseup"; touch = event.changedTouches[0];
-    }
-    newEvt.initMouseEvent(type, true, true, event.originalTarget.ownerDocument.defaultView, 0,
-        touch.screenX, touch.screenY, touch.clientX, touch.clientY,
-        evt.ctrlKey, evt.altKey, evt.shirtKey, evt.metaKey, 0, null);
-    event.originalTarget.dispatchEvent(newEvt);
-    iX.textContent = event.clientX
-    iY.textContent = event.clientY
-    iEvent.textContent = event.type
+const handleStart = (event) => {
+    let x = event.touches[0].clientX
+    let y = event.touches[0].clientY
+    iX.textContent = x
+    iY.textContent = y
+    iEvent.textContent = "touchstart"
 }
-  
+const handleMove = (event) => {
+    iEvent.textContent = "touchemove"
+}
+
+const handleCancel = (event) => {
+    iEvent.textContent = "touchcancel"
+}
+
+const handleEnd = (event) => {
+    iEvent.textContent = "touchend"
+} 
 
 document.addEventListener("DOMContentLoaded", () =>  {
-    document.addEventListener('touchstart', onTouch)
-    document.addEventListener('touchmove', onTouch)
-    document.addEventListener('touchcancel', onTouch)
-    document.addEventListener('touchend', onTouch)
+    document.addEventListener('touchstart', handleStart)
+    document.addEventListener('touchmove', handleMove)
+    document.addEventListener('touchcancel', handleCancel)
+    document.addEventListener('touchend', handleEnd)
 } )
