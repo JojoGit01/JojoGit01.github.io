1d6b63251eab797b6e844d9d2e19d9e1f041bb9e
Jonathan <jonathan-08@orange.fr>
2022-01-08 15:39:31 +0100

    Init exo 4

---
+++ CartoTD1/Exo4/index.js
@@ -0,0 +1,34 @@
+const start = () => { 
+
+}
+
+const iEvent = document.querySelector('#event')
+
+const getPosClicked = (event) => {
+    const iX = document.querySelector('#x')
+    const iY = document.querySelector('#y')
+
+    iX.textContent = event.pageX
+    iY.textContent = event.pageY
+}
+
+const onTouch = (event) => {
+    event.preventDefault();
+    
+    switch (event.type) {
+        case "touchestart": iEvent.textContent = "touchestart"
+        case "touchmove": iEvent.textContent = "touchmove"
+        case "touchcancel": iEvent.textContent = "touchcancel"
+        case "touchend": iEvent.textContent = "touchend"
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () =>  {
+
+    document.addEventListener('click', getPosClicked)
+
+    document.addEventListener('touchstart', onTouch)
+    document.addEventListener('touchmove', onTouch)
+    document.addEventListener('touchcancel', onTouch)
+    document.addEventListener('touchend', onTouch)
+} )
